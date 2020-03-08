a392c8abe287fa3a52f070ada9e74730e3e789d9
kon3ktor <szekeres.sandor.88@gmail.com>
2020-03-08 15:40:01 +0100

    myFirstJavaApp

---
+++ src/com/beginner/yourfirstjavacode/MyFirstJavaApp.java
@@ -0,0 +1,8 @@
+package com.beginner.yourfirstjavacode;
+
+public class MyFirstJavaApp {
+
+    public static void main(String[] args) {
+        System.out.println("May the force be with you!");
+    }
+}
