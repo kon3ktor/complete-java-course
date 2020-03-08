8c0cd6bb57dda739ff840d4bf3631fdc126e86da
kon3ktor <szekeres.sandor.88@gmail.com>
2020-03-08 15:27:50 +0100

    Delete Main.java

--- src/com/hello/Main.java
+++
@@ -1,8 +0,0 @@
-package com.hello;
-
-public class Main {
-
-    public static void main(String[] args) {
-	    System.out.println("With HyperLoop it takes about " + 9900/1200 + " minutes to get from Paris to New York.");
-    }
-}
