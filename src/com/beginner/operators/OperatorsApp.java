f19f3c630069f11bc773619701fd2a4448fa8113
szekeres.sandor <szekeres.sandor.88@gmail.com>
2021-08-01 08:29:47 +0200

    OperatorsAppUpdate

--- src/com/beginner/operators/OperatorsApp.java
+++ src/com/beginner/operators/OperatorsApp.java
@@ -1,24 +1,26 @@
 package com.beginner.operators;
 
 public class OperatorsApp {
+
     public static void main(String[] args) {
+
         String carModel = "Dodge Challenger SRT 392";
         int price = 14999;
-        int moneyInTheBank = 1000000;
+        int moneyInTheBank = 100000;
         boolean isDamaged = true;
 
+        System.out.println("Price of a " + carModel + ": €" + price);
 
-        System.out.println("Price: €" + price);
-        int priceIncreased = price + 1000;
-        System.out.println("Increased price: €" + priceIncreased);
-        int priceDecreased = price - 1000;
-        System.out.println("Decreased price: €" + priceDecreased);
-        int priceTwoCars = price * 2;
-        System.out.println("Two car's price: €" + priceTwoCars);
+        int increasedPrice = price + 1000;
+        System.out.println("The increased price of a " + carModel + ": €" + increasedPrice);
+        int decreasedPrice = price - 1000;
+        System.out.println("The decreased price of a " + carModel + ": €" + decreasedPrice);
+        int twoCarsPrice = price * 2;
+        System.out.println("Two " + carModel + ": €" + twoCarsPrice);
         int dodgesYouCanBuy = moneyInTheBank / price;
-        System.out.println("Dodged you can buy for you money: " + dodgesYouCanBuy);
-        int moneyRemain = moneyInTheBank % price;
-        System.out.println("Money I'll have after buying " + dodgesYouCanBuy + " Dodge Challenger: €" + moneyRemain);
+        System.out.println("From the money we have in the bank we can buy " + dodgesYouCanBuy + " " + carModel);
+        int moneyRemaining = moneyInTheBank % price;
+        System.out.println("Money we would remain after buying " + dodgesYouCanBuy + " " + carModel + ": €" + moneyRemaining);
         System.out.println();
 
         int priceNegative = -14999;
@@ -26,46 +28,41 @@ public class OperatorsApp {
         System.out.println("Negative price with plus sign: €" + priceNegativeWithPlusSign);
         int priceNegativeWithMinusSign = -priceNegative;
         System.out.println("Negative price with minus sign: €" + priceNegativeWithMinusSign);
-
         int priceOneEuroIncrease = ++price;
-        System.out.println("Price after one € price increase: €" + priceOneEuroIncrease);
-        System.out.println("Original price after one € price increase: €" + price);
+        System.out.println("Price after 1 euro price increase: €" + priceOneEuroIncrease);
         int priceOneEuroDecrease = --price;
-        System.out.println("Price after one € price decrease: €" + priceOneEuroDecrease);
-        System.out.println("Original price after one € price decrease: €" + price);
-
-        System.out.println("The car is damaged: " + !isDamaged);
+        System.out.println("Price after 1 euro price decrease: €" + priceOneEuroDecrease);
+        System.out.println("This car is damaged: " + !isDamaged);
         System.out.println();
 
-        System.out.println("Car's price equals to the money in the bank: " + (price == moneyInTheBank));
-        System.out.println("Car's price not equals to the money in the bank: " + (price != moneyInTheBank));
-        System.out.println("Car's price greater than the money in the bank: " + (price > moneyInTheBank));
-        System.out.println("Car's price lesser than the money in the bank: " + (price < moneyInTheBank));
-        System.out.println("Car's price greater than or equals to the money in the bank: " + (price >= moneyInTheBank));
-        System.out.println("Car's price lesser than or equals to the money in the bank: " + (price <= moneyInTheBank));
-        System.out.println("Car's model variable is a String: " + (carModel instanceof String));
+        System.out.println("Car's price equals the money in the bank: " + (price == moneyInTheBank));
+        System.out.println("Car's price doesn't equal the money in the bank: " + (price != moneyInTheBank));
+        System.out.println("Car's price is greater than the money in the bank: " + (price > moneyInTheBank));
+        System.out.println("Car's price is lesser than the money in the bank: " + (price < moneyInTheBank));
+        System.out.println("Car's price is greater than or equals the money in the bank: " + (price >= moneyInTheBank));
+        System.out.println("Car's price is lesser or equals than the money in the bank: " + (price <= moneyInTheBank));
+        System.out.println("The carModel variable's datatype is a String: " + (carModel instanceof String));
         System.out.println();
 
-
-        String damagedText = !isDamaged ? "The car is damaged." : "The car isn't damaged.";
+        String damagedText = isDamaged ? "The car is damaged" : "The car isn't damaged";
         System.out.println(damagedText);
         System.out.println();
 
-        String worthSeeingMessage = !isDamaged || price < 20000 ? "It's worth seeing the car." : "It's not worth seeing the car.";
-        System.out.println(worthSeeingMessage);
-        String worthRepairing = isDamaged && price < 15000 ? "It's worth repairing the car." : "It's not worth repairing the car.";
-        System.out.println(worthRepairing);
-        System.out.println();
+        String worthSeeingText = !isDamaged || price <= 20000 ? "It's worth seeing the car" : "It isn't worth seeing the car";
+        System.out.println(worthSeeingText);
+        String worthRepairingText = isDamaged && price <= 10000 ? "It's worth repairing the car" : "It isn't worth repairing the car";
+        System.out.println(worthRepairingText);
 
         price += 1000;
-        System.out.println("Increased price: €" + price);
-        price -= 1000;
-        System.out.println("Decreased price: €" + price);
+        System.out.println("Price increased: €" + price);
+        price -= 2000;
+        System.out.println("Price decreased: €" + price);
         price *= 2;
-        System.out.println("Doubled price: €" + price);
+        System.out.println("Price multiplied: €" + price);
         price /= 2;
-        System.out.println("Half price: €" + price);
+        System.out.println("Price divided: €" + price);
         price %= 10000;
-        System.out.println("Remained price: €" + price);
+        System.out.println("Price remained: €" + price);
+
     }
 }
